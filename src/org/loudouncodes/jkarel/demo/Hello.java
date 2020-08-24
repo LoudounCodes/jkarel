d1e5ea15ea8bf4eff8b2a2568eebc3e083a2e51d
David Bock <dbock@javaguy.org>
2020-08-24 09:51:11 -0400

    adds the beginnings of a hello world robot that run as the jar file.

--- src/org/loudouncodes/jkarel/demo/Hello.java
+++ src/org/loudouncodes/jkarel/demo/Hello.java
@@ -1,10 +1,16 @@
 package org.loudouncodes.jkarel.demo;
 
+import org.loudouncodes.jkarel.*;
+
 public class Hello {
   
   public static void main(String[] args) {
     
     System.out.println("Hello World!");
+    
+    Arena.openWorld("/org/loudouncodes/jkarel/demo/hello.map");
+		Arena.getWorldFrame().setSize(800, 230);
+    
   }
   
 }
