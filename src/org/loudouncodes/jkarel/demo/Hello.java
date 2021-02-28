1d6b083a9d5a67170505966f83f7a4c9449cbe32
David Bock <dbock@javaguy.org>
2021-02-27 19:21:58 -0500

    adds some javadoc

--- src/org/loudouncodes/jkarel/demo/Hello.java
+++ src/org/loudouncodes/jkarel/demo/Hello.java
@@ -2,16 +2,27 @@ package org.loudouncodes.jkarel.demo;
 
 import org.loudouncodes.jkarel.*;
 
+/*
+ * main entry point for the 'hello world' Karel demo.
+ * this should run if you run the jar file built with
+ * the project's ant build system.
+ *
+ * This creates an Arena, makes it large enough to
+ * display the message, sets teh speed to 'ludicrus',
+ * instantiates an AlphaBot, and makes it say 'Hello World'.
+ *
+ * @author David Bock (dbock@loudouncodes.org)
+ */
 public class Hello {
   
   public static void main(String[] args) {
-        
     Arena.openWorld("/org/loudouncodes/jkarel/demo/hello.map");
-		Arena.getArenaFrame().setSize(1024, 250);
-    
+	Arena.getArenaFrame().setSize(1024, 250);
     Arena.setPace(Pacing.LUDICRUS);
-    AlphaBot bot = new AlphaBot();
+
+
+	AlphaBot bot = new AlphaBot();
     bot.say("Hello World!"); 
   }
-  
+
 }
