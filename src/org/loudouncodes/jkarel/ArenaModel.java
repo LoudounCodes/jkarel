de7b56007fc3519466d5503267b06b7c8469fd7e
David Bock <dbock@javaguy.org>
2021-03-09 21:42:01 -0500

    revisits the ugly in this file.

--- src/org/loudouncodes/jkarel/ArenaModel.java
+++ src/org/loudouncodes/jkarel/ArenaModel.java
@@ -1,5 +1,7 @@
 package org.loudouncodes.jkarel;
 
+// breaks an important design rule with knowledge of a sub-package.
+// need to fix that.
 import org.loudouncodes.jkarel.xml.*;
 
 import java.io.*;
@@ -7,16 +9,18 @@ import java.net.*;
 import java.util.*;
 
 /**
- * As I have been refactoring the FCPS version of this code, a lot of
- * complexity has been pushed into this class.  It will eventually
- * be given the same treatment as the rest of the complexity and removed.
- *
- * Represents a back-end 'model' of the area; the locations of all the
+ * <p>Represents a back-end 'model' of the area; the locations of all the
  * walls, beepers, and robots are stored here.  In the future, it will
- * also be responsible for notifying ArenaListeners of ArenaEvents.
+ * also be responsible for notifying ArenaListeners of ArenaEvents.</p>
+ *
+ * <p>As I have been refactoring the FCPS version of this code, a lot of
+ * complexity has been pushed into this class.  It will eventually
+ * be given the same treatment as the rest of the complexity and removed.</p>
+ *
  */
 public class ArenaModel {
 
+    /** singletons all the way down the stack. This is ugly. */
 	private static ArenaModel current = null;
 
 	private Map<Location, BeeperStack> beepers;
@@ -40,6 +44,8 @@ public class ArenaModel {
 		walls.add(xAxisWall = new Wall(1, 0, Arena.HORIZONTAL));
 		walls.add(yAxisWall = new Wall(0, 1, Arena.VERTICAL));
 
+        // arenamodel needs to be a true model object. parsing xml
+        // is not the responsibility of a model, especially in a constructor.
 		parseMap(mapName);
 	}
 
@@ -47,6 +53,41 @@ public class ArenaModel {
 		this(null);
 	}
 
+    // getters
+	Map<Location, BeeperStack> getBeepers() {
+		return beepers;
+	}
+
+	List<Wall> getWalls() {
+		return walls;
+	}
+
+	List<Robot> getRobots() {
+		return robots;
+	}
+    
+	public Location getSize() {
+		return new Location(width, height);
+	}
+
+	/**
+	 * Returns the currently running instance of ArenaModel.
+     * Ew. singleton pattern.  This won't do in the new world order...
+	 */
+	public static ArenaModel getCurrent() {
+		return current;
+	}
+    
+    /**
+      * cleans up our singleton reference when the model is no longer needed.
+      */
+	void close() {
+		current = null;
+	}
+    
+    
+    
+    
 	void addRobot(Robot r) {
 		synchronized (robots) {
 			robots.add(r);
@@ -90,6 +131,70 @@ public class ArenaModel {
 			walls.add(w);
 		}
 	}
+
+
+
+	boolean checkBeepers(Location l) {
+		return beepers.get(l) != null;
+	}
+
+	boolean isNextToARobot(Robot r, Location l) {
+		synchronized (robots) {
+			for (Robot robot : robots)
+				if (robot != r && robot.getX() == l.getX() && robot.getY() == l.getY())
+					return true;
+
+			return false;
+		}
+	}
+
+    // need to add border walls back
+	void setSize(int width, int height) {
+		if (this.width != width) {
+			this.width = width;
+			walls.remove(xAxisWall);
+			walls.add(xAxisWall = new Wall(1, 0, Arena.HORIZONTAL));
+		}
+
+		if (this.height != height) {
+			this.height = height;
+			walls.remove(yAxisWall);
+			walls.add(yAxisWall = new Wall(0, 1, Arena.VERTICAL));
+		}
+	}
+
+    // this needs some deobfuscation.
+	boolean checkWall(int x, int y, int orientation) {
+		synchronized (walls) {
+			switch (orientation) {
+				case Arena.HORIZONTAL:
+					for (Wall w : walls)
+						if (w.getOrientation() == orientation)
+							if (w.getY() == y &&
+							                x >= w.getX() &&
+							                x < w.getX() + 1)
+								return true;
+
+					break;
+				case Arena.VERTICAL:
+				default:
+					for (Wall w : walls)
+						if (w.getOrientation() == orientation)
+							if (w.getX() == x &&
+							                y >= w.getY() &&
+							                y < w.getY() + 1)
+								return true;
+
+					break;
+			}
+		}
+		return false;
+	}
+
+
+
+
+    // stuff having to do with xml parsing
 
 	public void addObject_beeper(Attributes a) {
 		int x = Integer.parseInt(a.get("x"));
@@ -135,11 +240,9 @@ public class ArenaModel {
 		WorldParser.initiateMap(e);
 	}
 
-	private InputStream getInputStreamForMap(String fileName) {
-		
-    InputStream mapSource = null;
-
-		try {
+	private InputStream getInputStreamForMap(String fileName) {	
+      InputStream mapSource = null;
+      try {
 			if (fileName == null)
 				throw new FileNotFoundException();
       
@@ -165,90 +268,5 @@ public class ArenaModel {
 		}
 
 		return mapSource;
-	}
-
-	Map<Location, BeeperStack> getBeepers() {
-		return beepers;
-	}
-
-	List<Wall> getWalls() {
-		return walls;
-	}
-
-	List<Robot> getRobots() {
-		return robots;
-	}
-
-    // this needs some deobfuscation.
-	boolean checkWall(int x, int y, int orientation) {
-		synchronized (walls) {
-			switch (orientation) {
-				case Arena.HORIZONTAL:
-					for (Wall w : walls)
-						if (w.getOrientation() == orientation)
-							if (w.getY() == y &&
-							                x >= w.getX() &&
-							                x < w.getX() + 1)
-								return true;
-
-					break;
-				case Arena.VERTICAL:
-				default:
-					for (Wall w : walls)
-						if (w.getOrientation() == orientation)
-							if (w.getX() == x &&
-							                y >= w.getY() &&
-							                y < w.getY() + 1)
-								return true;
-
-					break;
-			}
-		}
-		return false;
-	}
-
-	boolean checkBeepers(Location l) {
-		return beepers.get(l) != null;
-	}
-
-	boolean isNextToARobot(Robot r, Location l) {
-		synchronized (robots) {
-			for (Robot robot : robots)
-				if (robot != r && robot.getX() == l.getX() && robot.getY() == l.getY())
-					return true;
-
-			return false;
-		}
-	}
-
-    // need to add border walls back
-	void setSize(int width, int height) {
-		if (this.width != width) {
-			this.width = width;
-			walls.remove(xAxisWall);
-			walls.add(xAxisWall = new Wall(1, 0, Arena.HORIZONTAL));
-		}
-
-		if (this.height != height) {
-			this.height = height;
-			walls.remove(yAxisWall);
-			walls.add(yAxisWall = new Wall(0, 1, Arena.VERTICAL));
-		}
-	}
-
-	public Location getSize() {
-		return new Location(width, height);
-	}
-
-	void close() {
-		current = null;
-	}
-
-	/**
-	 * Returns the currently running instance of ArenaModel.
-     * Ew. singleton pattern.  This won't do in the new world order...
-	 */
-	public static ArenaModel getCurrent() {
-		return current;
 	}
 }
