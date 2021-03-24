a8a3f798f3a8f872e60bce778f13db48f6922557
David Bock <dbock@javaguy.org>
2021-03-23 22:34:28 -0400

    adds our first sample passing unit test.

---
+++ src/test/org/loudouncodes/jkarel/ArenaTests.java
@@ -0,0 +1,24 @@
+package org.loudouncodes.jkarel;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+
+class ArenaTests {
+
+
+	@Test
+	@DisplayName("Asserts true")
+	void simplePass() {
+		assertEquals(true, true, "if this fails, we have bigger problems...");
+	}
+    
+    
+    
+}
